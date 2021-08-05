1df9184518e70d46f9208461b4382406dca87a63
Kevin Germain <contact@kevin-germain.fr>
2021-08-05 15:11:56 +0200

    setting up firebase - begin upload form

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -7,7 +7,7 @@ const Title = () => {
       <h2>Your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
---
+++ src/firebase/config.exemple.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// You can find these informations when creating the firebase project
+const firebaseConfig = {
+  apiKey: 'YOUR API KEY',
+  authDomain: 'YOUR AUTHDOMAIN',
+  projectId: 'YOUR PROJECT ID',
+  storageBucket: 'YOUR STORAGE BUCKET',
+  messagingSenderId: 'YOUR MESSAGING SENDER ID',
+  appId: 'YOUR APP ID',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
