860f81677af8167b8a4f730da4a8f80b306fc035
Kevin Germain <contact@kevin-germain.fr>
2021-08-06 11:57:43 +0200

    image grid + style

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -3,11 +3,14 @@ import useFirestore from '../hooks/useFirestore';
 
 const ImageGrid = () => {
   const { docs } = useFirestore('images');
-  console.log(docs)
 
   return (
     <div className="img-grid">
-      image
+      { docs && docs.map((doc) => (
+        <div className="img-wrap" key={doc.id}>
+          <img src={doc.url} alt="uploaded pic" />
+        </div>
+      ))}
     </div>
   );
 };
